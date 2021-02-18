c7ddc3c348939e30619656c36c3782a2b3fe075d
DilrubaS <71407801+dilruba@users.noreply.github.com>
2021-02-18 21:58:36 +0300

    first changes

--- src/test/java/com/azulcrm/step_definitions/MyProfileStepDefs.java
+++ src/test/java/com/azulcrm/step_definitions/MyProfileStepDefs.java
@@ -1,4 +1,39 @@
 package com.azulcrm.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class MyProfileStepDefs {
+
+    @Given("the user logs in with valid username and password")
+    public void the_user_logs_in_with_valid_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user clicks on {string} in the Portal Page")
+    public void the_user_clicks_on_in_the_Portal_Page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user clicks on {string} in the User Menu")
+    public void the_user_clicks_on_in_the_User_Menu(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should be able to display Edit Profile Settings window")
+    public void the_user_should_be_able_to_display_Edit_Profile_Settings_window() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should be able to display Contact Information")
+    public void the_user_should_be_able_to_display_Contact_Information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
