879a05f8f84ffa5b5f00cf4c3dc8837cb4e950c6
mmyilmaz <m.mahmutyilmaz@gmail.com>
2021-02-23 20:25:01 +0100

    tests passed
    .

--- src/test/java/com/azulcrm/step_definitions/WorkgroupsStepDefs.java
+++ src/test/java/com/azulcrm/step_definitions/WorkgroupsStepDefs.java
@@ -2,9 +2,15 @@ package com.azulcrm.step_definitions;
 
 import com.azulcrm.pages.WorkGroupsPage;
 import com.azulcrm.utilities.BrowserUtils;
+import com.azulcrm.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkgroupsStepDefs {
 
@@ -13,18 +19,36 @@ public class WorkgroupsStepDefs {
     @When("the user clicks on join button under the groups")
     public void the_user_clicks_on_join_button_under_the_groups() {
         BrowserUtils.waitForClickablility(new WorkGroupsPage().joinButton,5);
-       new WorkGroupsPage().joinButton.click();
+        BrowserUtils.waitFor(1);
+        BrowserUtils.clickWithJS(new WorkGroupsPage().joinButton);
+       //new WorkGroupsPage().joinButton.submit();
+       /*
+       List<WebElement> myList=new ArrayList<>();
+       String forWEJoinButton= "#bx-sonet-groups-request-";
+       int i=1;
+       while (true) {
+           String forWEButtonFinal=forWEJoinButton+i;
+           WebElement w=Driver.get().findElement(By.cssSelector("#"+forWEButtonFinal));
+           if(w.isDisplayed()) {
+               myList.add(w);
+           }
+       }
+
+        */
     }
 
     @Then("A message as {string} should be displayed")
     public void a_message_as_should_be_displayed(String string) {
-        String actual= new WorkGroupsPage().textAfterJoin.getText();
+        BrowserUtils.waitFor(1);
+        String actual= Driver.get().findElement(By.xpath("//*[@id=\"bx-sonet-groups-request-sent-8\"]")).getText();
+        System.out.println(actual);
+        System.out.println(string);;
         Assert.assertTrue(string.equals(actual));
     }
 
     @When("the user clicks on the star next to the group name")
     public void the_user_clicks_on_the_star_next_to_the_group_name() {
-        BrowserUtils.waitForClickablility(new WorkGroupsPage().star,5);
+      BrowserUtils.waitForClickablility(new WorkGroupsPage().star,5);
         new WorkGroupsPage().star.click();
 
     }
--- target/test-classes/features/Workgroups.feature
+++ target/test-classes/features/Workgroups.feature
@@ -1,9 +1,9 @@
 
 Feature: Workgroups function on Activity Stream
-  Background:
-    Given the user logs in using following credentials "userName" and "password"
-    When the user navigates to "Workgroups" module
-
+  //Background:
+    //Given the user logs in using following credentials "userName" and "password"
+    //When the user navigates to "Workgroups" module
+  @wip
   Scenario: Join to the groups
     When the user clicks on join button under the groups
     Then A message as "Request has been sent" should be displayed
