187d79c5f53262bf3cc9c69531adf6d058e43fc1
ismail dundar <dundar.sml@gmail.com>
2021-02-23 17:36:12 +0100

    login and drive function

--- src/test/java/com/azulcrm/step_definitions/DriveFunctionsStepDefs.java
+++ src/test/java/com/azulcrm/step_definitions/DriveFunctionsStepDefs.java
@@ -10,8 +10,7 @@ public class DriveFunctionsStepDefs {
         // Write code here that turns the phrase above into concrete actions
         // For automatic transformation, change DataTable to one of
         // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        // 
         //
         // For other transformations you can register a DataTableType.
         throw new io.cucumber.java.PendingException();
