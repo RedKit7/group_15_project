ad40c6f84f06d91cb5db9dacda7d447e28cbc295
kareemgunner <gunnerkareem@gmail.com>
2021-02-23 18:48:28 +0100

    Step_Definitions added

--- src/test/java/com/azulcrm/pages/MyCalendarPage.java
+++ src/test/java/com/azulcrm/pages/MyCalendarPage.java
@@ -29,12 +29,6 @@ public class MyCalendarPage extends BasePage{
     @FindBy (xpath = "//h2[@class='calendar-top-title']")
     public WebElement calendarTitle;
 
-    @FindBy (xpath = "//span[@class='calendar-day-of-week-day']")
-    public WebElement dailyCalendarWeekDay;
-
-    @FindBy (xpath = "//div[@class='calendar-grid-week-row-days-week']")
-    public WebElement weeklyCalendarDaysRow;
-
     @FindBy (xpath = "//input[@id='calendar_slider_696528_entry_name']")
     public WebElement newEventName;
 
