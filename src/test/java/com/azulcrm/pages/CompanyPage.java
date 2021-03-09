caea1010efb762627e2e9b69b6480462fa59db99
Dilruba Sarican <diddllina_686@hotmail.de>
2021-03-09 03:34:45 +0300

    some changes

--- src/test/java/com/azulcrm/pages/CompanyPage.java
+++ src/test/java/com/azulcrm/pages/CompanyPage.java
@@ -1,4 +1,47 @@
 package com.azulcrm.pages;
 
+import com.azulcrm.utilities.BrowserUtils;
+import com.azulcrm.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CompanyPage extends BasePage{
+
+    public CompanyPage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+
+    @FindBy(id = "tab_cont_cedit1")
+    public WebElement previewText;
+
+    @FindBy(xpath = "//div[contains(text(),'RSS Feed')]")
+    public WebElement RssFeedHeaderTitle;
+
+
+
+
+    public void navigateToCompanyTabs(String tab){
+        String tabLocator = "//span[normalize-space()='"+tab+"' and contains(@class,'main-buttons-item-text-title')]";
+
+        BrowserUtils.waitForClickablility(By.xpath(tabLocator), 5);
+        WebElement tabElement = Driver.get().findElement(By.xpath(tabLocator));
+        tabElement.click();
+    }
+
+
+    public void companyNews(String Button){
+        String buttonLocator = "//span[normalize-space()='"+Button+"' and contains(@class,'webform-small-button-text')]";
+
+        BrowserUtils.waitForClickablility(By.xpath(buttonLocator), 5);
+        WebElement tabElement = Driver.get().findElement(By.xpath(buttonLocator));
+        tabElement.click();
+
+    }
+
+
+
+
 }
---
+++ src/test/java/com/azulcrm/step_definitions/CompanyStepDefs.java
@@ -0,0 +1,62 @@
+package com.azulcrm.step_definitions;
+
+import com.azulcrm.pages.CompanyPage;
+import com.azulcrm.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class CompanyStepDefs {
+
+
+    CompanyPage pageObject = new CompanyPage();
+
+    @When("the user clicks on {string} button in Company Page")
+    public void the_user_clicks_on_button_in_Company_Page(String string) {
+
+        pageObject.companyNews(string);
+
+    }
+
+    @Then("the user should be able to see Add News Pop-up Window")
+    public void the_user_should_be_able_to_see_Add_News_Pop_up_Window() {
+
+        Assert.assertTrue(pageObject.previewText.isDisplayed());
+
+    }
+
+    @When("the user clicks on {string} tab in Company Page")
+    public void the_user_clicks_on_tab_in_Company_Page(String string) {
+
+        new CompanyPage().navigateToCompanyTabs(string);
+
+    }
+
+    @Then("the user should be able to see {string} tab in About Company Window")
+    public void the_user_should_be_able_to_see_History_tab_in_About_Company_Window(String string) {
+
+        String firstUrl = Driver.get().getCurrentUrl();
+        pageObject.navigateToCompanyTabs(string);
+        Assert.assertNotEquals(Driver.get().getCurrentUrl(),firstUrl);
+
+    }
+
+    @Then("the user should be able to click {string} button in the upper right corner in Company Tabs")
+    public void the_user_should_be_able_to_click_button_in_the_upper_right_corner_in_Company_Tabs(String string) {
+
+        pageObject.companyNews(string);
+
+    }
+
+    @Then("the user should be able to see RSS Feed as header title")
+    public void the_user_should_be_able_to_see_RSS_Feed_as_header_title() {
+
+        Assert.assertTrue(pageObject.RssFeedHeaderTitle.isDisplayed());
+
+    }
+
+
+
+
+
+}
