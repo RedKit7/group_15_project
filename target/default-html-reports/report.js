8c1ee02a489d8376ab3dcf37e673c08eb4e851a2
Dilruba Sarican <diddllina_686@hotmail.de>
2021-03-09 19:05:52 +0300

    latest changes

--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -38,6 +38,70 @@ formatter.result({
   "status": "passed"
 });
 formatter.scenario({
+  "name": "User can display \"News\" window by clicking on \"News\" button.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Company"
+    },
+    {
+      "name": "@AZ-189"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user clicks on \"News\" button in Company Page",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_clicks_on_button_in_Company_Page(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "page title should be \"Important information\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.MyProfileStepDefs.page_title_should_be(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials \"human_resource_username\" and \"password\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.LoginStepDefs.the_user_logs_in_using_following_credentials_and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigates to \"Company\" module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.TasksStepDefs.the_user_navigates_to_module(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
   "name": "User can display \"Add News\" window by clicking on \"Add News\" button.",
   "description": "",
   "keyword": "Scenario",
@@ -66,6 +130,454 @@ formatter.step({
 });
 formatter.match({
   "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_should_be_able_to_see_Add_News_Pop_up_Window()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials \"human_resource_username\" and \"password\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.LoginStepDefs.the_user_logs_in_using_following_credentials_and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigates to \"Company\" module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.TasksStepDefs.the_user_navigates_to_module(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User can display \"Official Information\" by clicking on \"Official Information\" tab.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Company"
+    },
+    {
+      "name": "@AZ-191"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user clicks on \"Official Information\" tab in Company Page",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_clicks_on_tab_in_Company_Page(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "page title should be \"Company\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.MyProfileStepDefs.page_title_should_be(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials \"human_resource_username\" and \"password\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.LoginStepDefs.the_user_logs_in_using_following_credentials_and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigates to \"Company\" module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.TasksStepDefs.the_user_navigates_to_module(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User can see \"Our Life\" window by clicking on \"Our Life\" tab.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Company"
+    },
+    {
+      "name": "@AZ-192"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user clicks on \"Our Life\" tab in Company Page",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_clicks_on_tab_in_Company_Page(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "page title should be \"Company life\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.MyProfileStepDefs.page_title_should_be(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials \"human_resource_username\" and \"password\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.LoginStepDefs.the_user_logs_in_using_following_credentials_and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigates to \"Company\" module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.TasksStepDefs.the_user_navigates_to_module(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User can see \"About Company\" window by clicking on \"About Company\" Tab.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Company"
+    },
+    {
+      "name": "@AZ-193"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user clicks on \"About Company\" tab in Company Page",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_clicks_on_tab_in_Company_Page(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user should be able to see \"History\" tab in About Company Window",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_should_be_able_to_see_History_tab_in_About_Company_Window(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials \"human_resource_username\" and \"password\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.LoginStepDefs.the_user_logs_in_using_following_credentials_and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigates to \"Company\" module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.TasksStepDefs.the_user_navigates_to_module(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User can see an album and photos page by clicking \"Photo Gallery\" tab.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Company"
+    },
+    {
+      "name": "@AZ-194"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user clicks on \"Photo Gallery\" tab in Company Page",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_clicks_on_tab_in_Company_Page(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "page title should be \"Photo gallery\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.MyProfileStepDefs.page_title_should_be(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials \"human_resource_username\" and \"password\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.LoginStepDefs.the_user_logs_in_using_following_credentials_and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigates to \"Company\" module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.TasksStepDefs.the_user_navigates_to_module(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User can display video player window by clicking \"Video\" tab.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Company"
+    },
+    {
+      "name": "@AZ-195"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user clicks on \"Video\" tab in Company Page",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_clicks_on_tab_in_Company_Page(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user should be able to click \"Video\" button in the upper right corner in Company Tabs",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_should_be_able_to_click_button_in_the_upper_right_corner_in_Company_Tabs(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials \"human_resource_username\" and \"password\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.LoginStepDefs.the_user_logs_in_using_following_credentials_and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigates to \"Company\" module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.TasksStepDefs.the_user_navigates_to_module(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User can display vacancy button by clicking \"Career\" tab.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Company"
+    },
+    {
+      "name": "@AZ-196"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user clicks on \"Career\" tab in Company Page",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_clicks_on_tab_in_Company_Page(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user should be able to click \"Vacancies\" button in the upper right corner in Company Tabs",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_should_be_able_to_click_button_in_the_upper_right_corner_in_Company_Tabs(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials \"human_resource_username\" and \"password\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.LoginStepDefs.the_user_logs_in_using_following_credentials_and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user navigates to \"Company\" module",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.TasksStepDefs.the_user_navigates_to_module(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User can display the news under Business News(RSS) menu.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Company"
+    },
+    {
+      "name": "@AZ-197"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user clicks on \"Business News (RSS)\" tab in Company Page",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_clicks_on_tab_in_Company_Page(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user should be able to see RSS Feed as header title",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.azulcrm.step_definitions.CompanyStepDefs.the_user_should_be_able_to_see_RSS_Feed_as_header_title()"
 });
 formatter.result({
   "status": "passed"
